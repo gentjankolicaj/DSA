aceddabb43d4685ae4a73b3ef779b26efac74969
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2023-06-03 10:37:29 +0200

    first commit.

---
+++ dining-philosophers/src/main/java/org/dsa/dining_philosophers/App.java
@@ -0,0 +1,11 @@
+package org.dsa.dining_philosophers;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+
+    public static void main(String[] args) {
+        log.info("");
+    }
+}
